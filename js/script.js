a6980ecb688cb83df7acf5c2d39a7eb23fccf324
andriiafanasiev <andreynoobroo@gmail.com>
2023-05-12 20:05:20 +0200

    fix js

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ function countChange() {
     });
     counterIndicator.innerText = countInCart;
     if (countInCart === 0) {
-        counterIndicator.innerText = "0gi";
+        counterIndicator.innerText = "0";
     }
 }/*Func of changes in the counter of goods by entering in the Input */
 function inputCountChange() {
